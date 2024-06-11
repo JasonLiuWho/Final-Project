9ba2f917a97c0c690c7ce6a43a7710fb6dd62872
JasonLiuWho <jasonliu.business@gmail.com>
2024-06-11 15:22:39 -0500

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS finalProjectdb;
+CREATE DATABASE finalProjectdb;
+USE finalProjectdb;
+
+CREATE TABLE user (
+    user_id MEDIUMINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    fName VARCHAR(20) NOT NULL ,
+    lName VARCHAR(20) NOT NULL,
+    email VARCHAR(40) UNIQUE NOT NULL,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_user_id PRIMARY KEY (user_id)
+);
+
+CREATE TABLE manga (
+    manga_id MEDIUMINT UNSIGNED AUTO_INCREMENT NOT NULL, 
+    title VARCHAR(30) NOT NULL,
+    author VARCHAR(20) NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+    volume VARCHAR(10) NOT NULL,
+    mangaDesc VARCHAR(300) NOT NULL,
+    releasedDate VARCHAR(10) NOT NULL, 
+    publisher VARCHAR(20) NOT NULL,
+    price VARCHAR(10) NOT NULL,
+    imgUrl MEDIUMINT, 
+    CONSTRAINT pk_manga_id PRIMARY KEY (manga_id)
+)
+
